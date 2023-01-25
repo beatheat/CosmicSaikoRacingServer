eae77f5129c263a9d57973de90d55b2bb6c5e510
beatheat <oh@beathe.at>
2023-01-25 14:35:07 +0900

    Matching Server Basic Functions

--- MatchingServer/MatchingServer/MatchingServer.cs
+++ MatchingServer/MatchingServer/MatchingServer.cs
@@ -12,20 +12,28 @@ namespace MatchingServer
 {
     internal class MatchingServer
     {
+        public class MatchInfo
+        {
+            public string id;
+            public bool canceled = false;
+        }
+
         public EdenNetServer server;
         public Dictionary<int, string> room;
-        public Queue<string> matchQueue;
+        public Queue<MatchInfo> matchQueue;
+        public Dictionary<string, MatchInfo> matchMap;
 
         public MatchingServer(EdenNetServer server)
         {
             this.server = server;
             room = new Dictionary<int, string>();
-            matchQueue = new Queue<string>();
+            matchQueue = new Queue<MatchInfo>();
         }
 
         public void Run()
         {
             server.AddReceiveEvent("MatchMake", MatchMake);
+            server.AddReceiveEvent("CancelMatchMake", CancelMatchMake);
             server.AddResponse("CreateLobby", CreateLobby);
             server.AddResponse("DestroyLobby", DestroyLobby);
             server.AddResponse("GetRoomAddress", GetRoomAddress);
@@ -36,24 +44,31 @@ namespace MatchingServer
                 {
                     if (matchQueue.Count >= 4)
                     {
-                        List<string> group = new List<string>();
-                        string host = matchQueue.Dequeue();
-                        group.Add(host);
-                        if (!server.StillConnected(host))
-                            continue;
-                        server.Send("StartHost", host);
-
-                        for (int i = 0; i < 3; i++)
+                        List<MatchInfo> matchGroup = new List<MatchInfo>();
+                        for (int i = 0; i < 4; i++)
                         {
-                            string client = matchQueue.Dequeue();
-                            group.Add(client);
-                            if (!server.StillConnected(client))
-                            {
-                                foreach (string mem in group)
-                                    matchQueue.Enqueue(mem);
-                                continue;
-                            }
-                            server.Send("HostAddress", client, host);
+                            var clientInfo = matchQueue.Dequeue();
+                            if (!clientInfo.canceled && server.StillConnected(clientInfo.id))
+                                matchGroup.Add(clientInfo);
+                        }
+                        if (matchGroup.Count != 4)
+                        {
+                            foreach (var clientInfo in matchGroup)
+                                matchQueue.Enqueue(clientInfo);
+                            continue;
+                        }
+
+                        var hostAddress = StringToAddress(matchGroup[0].id);
+                        server.Send("StartHost", matchGroup[0].id);
+                        for (int i = 1; i < 4; i++)
+                        {
+                            server.Send("StartClient", matchGroup[i].id, hostAddress);
+                        }
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (matchMap.ContainsKey(matchGroup[i].id))
+                                matchMap.Remove(matchGroup[i].id);
+                            //server.DisconnectClient(matchGroup[i].id);
                         }
                     }
                     await Task.Delay(100);
@@ -73,7 +88,7 @@ namespace MatchingServer
 
         public EdenData CreateLobby(string client_id, EdenData data)
         {
-            if(room.Count >= 100000)
+            if (room.Count >= 100000)
             {
                 return new EdenData(-1);
             }
@@ -109,20 +124,38 @@ namespace MatchingServer
         public EdenData GetRoomAddress(string client_id, EdenData data)
         {
             if (data.type != EdenData.Type.SINGLE)
-                return new EdenData("wrong number");
+                return new EdenData(new EdenError("ERR:Unauthorized Access"));
             int roomNum = data.Get<int>();
-            var address = room[roomNum].Split(':');
-            var sendData = new Dictionary<string, object>
-            {
-                ["address"] = address[0],
-                ["port"] = Int32.Parse(address[1])
-            };
-            return new EdenData(sendData);
+            if (room.ContainsKey(roomNum))
+                return StringToAddress(room[roomNum]);
+            else
+                return new EdenData(new EdenError("ERR:Wrong Lobby Number"));
         }
 
         public void MatchMake(string client_id, EdenData data)
         {
-            matchQueue.Enqueue(client_id);
+            MatchInfo matchInfo = new MatchInfo { id = client_id, canceled = false };
+            matchQueue.Enqueue(matchInfo);
+            matchMap.Add(matchInfo.id, matchInfo);
+        }
+
+        public void CancelMatchMake(string client_id, EdenData data)
+        {
+            if(matchMap.ContainsKey(client_id))
+            {
+                matchMap[client_id].canceled = true;
+            }
+        }
+
+        public static EdenData StringToAddress(string address)
+        {
+            var splitAddress = address.Split(':');
+            var sendData = new Dictionary<string, object>
+            {
+                ["address"] = splitAddress[0],
+                ["port"] = Int32.Parse(splitAddress[1])
+            };
+            return new EdenData(sendData);
         }
 
     }
--- MatchingServer/MatchingServer/Program.cs
+++ MatchingServer/MatchingServer/Program.cs
@@ -22,6 +22,8 @@ namespace MatchingServer
             }
 
             matchingServer.Close();
+
+
         }
     }
 }

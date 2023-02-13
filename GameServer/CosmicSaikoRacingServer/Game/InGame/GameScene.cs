acd1fc61d8ff2b220cd704cb10755b718eeb8b36
beatheat <oh@beathe.at>
2023-02-14 01:56:03 +0900

    feat: 게임씬에서 발생하는 기본 프로토콜 기능 추가

--- GameServer/CosmicSaikoRacingServer/Game/InGame/GameScene.cs
+++ GameServer/CosmicSaikoRacingServer/Game/InGame/GameScene.cs
@@ -4,86 +4,291 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
-
 
 namespace CSRServer
 {
     internal class GameScene : Scene
     {
-        List<GamePlayer> playerList;
-        Dictionary<string, GamePlayer> playerMap;
+        #region Properties
+        // 게임에 접속한 플레이어를 리스트와 맵 두가지로 관리함
+        // clientId를 id값으로 playerMap에서 GamePlayer 검색하고 클라이언트에게는 playerList만 보내줌
+        private readonly List<GamePlayer> playerList;
+        private readonly Dictionary<string, GamePlayer> playerMap;
 
-        int turn = 0;
-        int time = 0;
-        Timer timer;
+        private MaintainStore maintainStore;
 
+        // 전체 턴 수
+        private int turn = 0;
+        // 예열 섹션에서 시간제한을 알리는 변수
+        private int time;
+        //Phase진행순서 PREHEAT => DEPART -> MAINTAIN
+        enum Phase
+        {
+            Preheat, Depart, Maintain
+        }
+        private Phase phase;
+
+        #endregion
+        #region Load Methods
         public GameScene(GameManager gameManager, EdenNetServer server) : base(gameManager, server)
         {
             playerList = new List<GamePlayer>();
             playerMap = new Dictionary<string, GamePlayer>();
+            maintainStore = null!;
         }
 
+        //로비 씬으로 부터 플레이어 정보를 받아옴
         public override void Load()
         {
+            if (passedData == null || passedData.ContainsKey("playerList") == false)
+            {
+                throw new Exception("GameScene - passedData is null or playerList does not exists");
+            }
             List<LobbyPlayer> lbPlayerList = (List<LobbyPlayer>)passedData["playerList"];
 
             foreach(LobbyPlayer lbPlayer in lbPlayerList)
             {
-                GamePlayer gamePlayer = new GamePlayer();
-                gamePlayer.id = lbPlayer.id;
-                gamePlayer.nickname = lbPlayer.nickname;
-                gamePlayer.clientId = lbPlayer.clientId;
+                GamePlayer gamePlayer = new GamePlayer(lbPlayer.clientId, playerList.Count, lbPlayer.nickname);
                 playerList.Add(gamePlayer);
                 playerMap.Add(gamePlayer.clientId, gamePlayer);
             }
 
-            InitGame();
-            timer = new Timer(GameThread, null, 0, 1000);
-            //Thread game = new Thread(GameThread);
-            //game.Start();
+            maintainStore = new MaintainStore(playerList.Count);
+            
+            server.AddResponse("UseCard", UseCard);
+            server.AddResponse("RollResource", RollResource);
+
+            server.AddResponse("RerollStore", RerollStore);
+            server.AddResponse("BuyExp", BuyExp);
+            server.AddResponse("BuyCard", BuyCard);
+            server.AddResponse("RemoveCard", RemoveCard);
+            
+            server.AddReceiveEvent("PreheatEnd", PreheatEnd);
+            server.AddReceiveEvent("DepartEnd", DepartEnd);
+
+            turn = 1;
+            PreheatStart();
+            Timer timer = new Timer(GameTimer, null, 0, 1000);
         }
 
         public override void Destroy()
         {
+            server.RemoveResponse("UseCard");
+            server.RemoveResponse("RollResource");
+            
+            server.RemoveResponse("RerollStore");
+            server.RemoveResponse("BuyExp");
+            server.RemoveResponse("BuyCard");
+            server.RemoveResponse("RemoveCard");
 
+            server.RemoveReceiveEvent("PreheatEnd");
+            server.RemoveReceiveEvent("DepartEnd");
         }
+        #endregion
         #region Game Methods
-        public void InitGame()
+        
+        private void PreheatStart()
         {
-            turn = 0;
             time = 99;
+            phase = Phase.Preheat;
+            foreach (var player in playerList)
+            {
+                player.RollResource(null);
+                player.DrawCard(player.drawCount);
+                player.turnReady = false;
+                player.resourceRerollCount = player.availableRerollCount;
+            }
+
+            //TurnStart로 대체하자 -> playerList, playerIndex, turn
+            var monitorPlayerList = GetMonitorPlayerList();
+            foreach (var player in playerList)
+            {
+                server.SendAsync("PreheatStart", player.clientId, new Dictionary<string, object>
+                {
+                    ["player"] = player,
+                    ["playerList"] = monitorPlayerList,
+                    ["turn"] = turn,
+                    ["timer"] = time
+                });
+            }
         }
-        public void GameThread(object state)
+
+        private void DepartStart()
         {
-            time--;
-            server.Broadcast("time", time);
+            phase = Phase.Depart;
+            time = 99;
+            foreach (var player in playerList)
+                player.turnReady = false;
+            server.BroadcastAsync("DepartStart", GetMonitorPlayerList());
+        }
+
+        private void MaintainStart()
+        {
+            phase = Phase.Maintain;
+            time = 99;
+            foreach (var player in playerList)
+            {
+                player.turnReady = false;
+                var storeCards = maintainStore.ShowRandomCards(player.index, player.level);
+                server.SendAsync("MaintainStart", player.clientId, storeCards);
+            }
+        }
+        
+        // 1초에 한번씩 실행함
+        private void GameTimer(object? sender)
+        {
+            if (time >= 0)
+            {
+                time--;
+                if(phase == Phase.Preheat)
+                    server.BroadcastAsync("PreheatTime", time);
+                else if(phase == Phase.Maintain)
+                    server.BroadcastAsync("MaintainTime", time);
+            }
+            else
+            {
+                if (phase == Phase.Preheat)
+                    DepartStart();
+                else if (phase == Phase.Depart)
+                    MaintainStart();                    
+                else if(phase == Phase.Maintain)
+                    PreheatStart();
+            }
+
+        }
+        
+        private List<GamePlayer> GetMonitorPlayerList()
+        {
+            List<GamePlayer> monitorPlayerList = new List<GamePlayer>();
+            foreach (var player in playerList)
+            {
+                monitorPlayerList.Add(player.CloneForMonitor());
+            }
+            return monitorPlayerList;
         }
 
         #endregion
-        #region Network Methods
-
-        void ChangeModuleChip()
+        #region Receive/Response Methods
+        
+        //예열턴
+        private EdenData UseCard(string clientId, EdenData data)
         {
-
+            if (phase == Phase.Preheat)
+            {
+                int useCardIndex = data.Get<int>();
+                GamePlayer player = playerMap[clientId];
+                object[]? result = player.UseCard(useCardIndex, out var effectType);
+                if (result == null)
+                {
+                    return new EdenData(new EdenError("UseCard - Card index is wrong"));
+                }
+                var response = new Dictionary<string, object> {["effectType"] = effectType!, ["results"] = result};
+                return new EdenData(response);
+            }
+            return new EdenData(new EdenError("UseCard - Phase is not Preheat-Phase"));
         }
 
-
-        void Ready()
+        private EdenData RollResource(string clientId, EdenData data)
         {
-
+            if (phase == Phase.Preheat)
+            {
+                List<int> resourceFixed = data.Get<List<int>>();
+                GamePlayer player = playerMap[clientId];
+                var result = player.RollResource(resourceFixed);
+                if (result.Count == 0)
+                    return new EdenData(new EdenError("RollResource - Reroll Count is 0"));
+                return new EdenData(result);
+            }
+            return new EdenData(new EdenError("RollResource - Phase is not Preheat-Phase"));
         }
+        
+        //정비턴
+        private EdenData RerollStore(string clientId, EdenData data)
+        {
+            if (phase == Phase.Maintain)
+            {
+                GamePlayer player = playerMap[clientId];
+                var storeCards = maintainStore.ShowRandomCards(player.index, player.level);
+                return new EdenData(storeCards);
+            }
+            return new EdenData(new EdenError("RerollStore - Phase is not Maintain-Phase"));
+        }
+
+        private EdenData BuyExp(string clientId, EdenData data)
+        {
+            if (phase == Phase.Maintain)
+            {
+                //구현필요
+            }
+            return new EdenData(new EdenError("BuyExp - Phase is not Maintain-Phase"));
+        }
+        
+        private EdenData BuyCard(string clientId, EdenData data)
+        {
+            if (phase == Phase.Maintain)
+            {
+                GamePlayer player = playerMap[clientId];
+                int buyIndex = data.Get<int>();
+                Card? buyCard = maintainStore.BuyCard(player.index, buyIndex);
+                if (buyCard == null)
+                {
+                    return new EdenData(new EdenError("BuyCard - Store card index is wrong"));
+                }
+                player.AddCardToDeck(buyCard);
+                return new EdenData(buyCard);
+            }
+            return new EdenData(new EdenError("BuyCard - Phase is not Maintain-Phase"));
+        }
+
+        private EdenData RemoveCard(string clientId, EdenData data)
+        {
+            if (phase == Phase.Maintain)
+            {
+                GamePlayer player = playerMap[clientId];
+                int removeIndex = data.Get<int>();
+                bool success = player.RemoveCardFromDeck(removeIndex);
+                if (success == false)
+                    return new EdenData(new EdenError("RemoveCard - Remove index is wrong"));
+                return new EdenData(player.deck);
+            }
+            return new EdenData(new EdenError("RemoveCard - Phase is not Maintain-Phase"));
+        }
+
+        //==============
+
+        private void PreheatEnd(string clientId, EdenData data)
+        {
+            if (phase == Phase.Preheat)
+            {
+                GamePlayer player = playerMap[clientId];
+                player.turnReady = true;
+
+                //모든 플레이어가 예열턴을 마쳤는지 체크
+                bool checkAllReady = true;
+                foreach (var p in playerList)
+                    checkAllReady = checkAllReady && p.turnReady;
+                if (checkAllReady)
+                    DepartStart();
+            }
+        }
+        
+        private void DepartEnd(string clientId, EdenData data)
+        {
+            if (phase == Phase.Depart)
+            {
+                GamePlayer player = playerMap[clientId];
+                player.turnReady = true;
+
+                //모든 플레이어가 예열턴을 마쳤는지 체크
+                bool checkAllReady = true;
+                foreach (var p in playerList)
+                    checkAllReady = checkAllReady && p.turnReady;
+                if (checkAllReady)
+                    MaintainStart();
+            }
+        }
+
 
         #endregion
-
-        //public List<Resource[]> RollResource()
-        //{
-        //    //List<Resource[]> resourceList = new List<Resource[]>();
-        //    //for(int i=0;i<4;i++)
-        //    //{
-        //    //    resourceList[i]
-        //    //}
-        //}
-
     }
 }

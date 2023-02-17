5bef0c3282a3de5e11f4a08dd5e015858146011a
beatheat <oh@beathe.at>
2023-02-17 16:14:53 +0900

    feat: Card의 발동조건 클래스 구현

---
+++ GameServer/CosmicSaikoRacingServer/Source/Game/Object/Card/CardCondition.cs
@@ -0,0 +1,82 @@
+﻿
+
+namespace CSRServer.Game
+{
+	internal class CardCondition
+	{
+		private readonly List<ResourceType> conditionList;
+		private readonly List<int> countList;
+		private readonly bool freeSameCondtion;
+		private readonly int freeSameCount;
+
+		public CardCondition(List<ResourceType> conditionList, List<int> countList)
+		{
+			this.conditionList = conditionList;
+			this.freeSameCondtion = false;
+			this.countList = countList;
+		}
+		
+		public CardCondition(int sameCount)
+		{
+			this.conditionList = null!;
+			this.countList = null!;
+			this.freeSameCondtion = true;
+			this.freeSameCount = sameCount;
+		}
+		
+		public bool Check(List<ResourceType> resource)
+		{
+			if (freeSameCondtion == false)
+			{
+				for (int i = 0; i < conditionList.Count; i++)
+				{
+					var resourceType = conditionList[i];
+					int count = 0;
+					foreach (var resourceElement in resource)
+					{
+						if (resourceType == resourceElement)
+							count++;
+					}
+					if (count >= countList[i])
+						return true;
+				}
+				return false;
+			}
+			else
+			{
+				//투페어 , 혹시 쓰리페어?
+				if (freeSameCount == 22)
+				{
+					int pairCount = 0;
+					foreach (var resourceType in Enum.GetValues<ResourceType>())
+					{
+						int count = 0;
+						foreach (var resourceElement in resource)
+						{
+							if (resourceType == resourceElement)
+								count++;
+						}
+						if (count >= 2)
+							pairCount++;
+					}
+					return pairCount >= 2;
+				}
+				else
+				{
+					foreach (var resourceType in Enum.GetValues<ResourceType>())
+					{
+						int count = 0;
+						foreach (var resourceElement in resource)
+						{
+							if (resourceType == resourceElement)
+								count++;
+						}
+						if (count >= freeSameCount)
+							return true;
+					}
+					return false;
+				}
+			}
+		}
+	}
+}

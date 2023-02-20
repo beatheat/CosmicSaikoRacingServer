71594f55d0b57eab3c382790da14946aff8eebcc
beatheat <oh@beathe.at>
2023-02-20 19:07:22 +0900

    fix: 버프 기능 클래스->메소드 모듈로 변경

--- GameServer/CosmicSaikoRacingServer/Source/Game/Object/Buff/Buff.cs
+++ GameServer/CosmicSaikoRacingServer/Source/Game/Object/Buff/Buff.cs
@@ -1,7 +1,49 @@
-﻿namespace CSRServer.Game
+﻿using System.Text.Json.Serialization;
+
+namespace CSRServer.Game
 {
-	public class Buff
+	internal class Buff
 	{
+		public enum Type
+		{
+			ELECTRIC_LEAK, PROLIFERATION, EXPOSURE, BREAK_DOWN, HIGH_EFFICIENCY, LOW_EFFICIENCY
+		}
+		public Type type { protected set; get; }
+		public int count;
+		public Dictionary<string, object> variables { private set; get; }
+		
+		[JsonIgnore]
+		public BuffEffect initEffect;
+		
+		public Buff(Type type, BuffEffect initEffect)
+		{
+			this.type = type;
+			this.initEffect = initEffect;
+			this.count = 1;
+			this.variables = new Dictionary<string, object>();
+		}
+
+		public void Init(GamePlayer player)
+		{
+			initEffect(this, player);
+		}
+
+		public void Release()
+		{
+			count = 0;
+		}
+		
+		public T? GetVariable<T>(string key)
+		{
+			if (variables.ContainsKey(key))
+				return (T) variables[key];
+			return default(T);
+		}
+
+		public Buff Clone()
+		{
+			return (Buff)this.MemberwiseClone();
+		}
 		
 	}
 }
---
+++ GameServer/CosmicSaikoRacingServer/Source/Game/Object/Buff/BuffManager.cs
@@ -0,0 +1,73 @@
+﻿namespace CSRServer.Game
+{
+	delegate void BuffEffect(Buff buff, GamePlayer player);
+	internal static class BuffManager
+	{
+		public static Dictionary<Buff.Type, Buff> CreateBuffDictionary()
+		{
+			Dictionary<Buff.Type, Buff> buffs = new Dictionary<Buff.Type, Buff>();
+			Add(buffs, Buff.Type.ELECTRIC_LEAK, ElectricLeak);
+			Add(buffs, Buff.Type.PROLIFERATION, Proliferation);
+			Add(buffs, Buff.Type.EXPOSURE, Exposure);
+			Add(buffs, Buff.Type.BREAK_DOWN, BreakDown);
+			Add(buffs, Buff.Type.HIGH_EFFICIENCY, HighEfficiency);
+			Add(buffs, Buff.Type.LOW_EFFICIENCY, LowEfficiency);
+			return buffs;
+		}
+
+		private static void Add(Dictionary<Buff.Type, Buff> buffs, Buff.Type type, BuffEffect effect)
+		{
+			buffs.Add(type, new Buff(type, effect));
+		}
+
+		private static void ElectricLeak(Buff buff, GamePlayer player)
+		{
+			if (buff.variables.ContainsKey("resourceLockIndexList"))
+				buff.variables.Add("resourceLockIndexList", new List<int>());
+			Random random = new Random();
+			int lockCount = buff.count > player.resourceCount ?  player.resourceCount : buff.count;
+			List<int> lockIndex = new List<int>(lockCount);
+			for (int i = 0; i < lockCount; i++)
+			{
+				int randomIndex;
+				do
+				{
+					randomIndex = random.Next(player.resourceCount);
+					lockIndex.Add(randomIndex);
+				} while (lockIndex.FindIndex(x => x == randomIndex) > 0);
+			}
+
+			buff.variables["resourceLockIndexList"] = lockIndex;
+		}
+		
+		private static void Proliferation(Buff buff, GamePlayer player)
+		{
+			if (buff.variables.ContainsKey("resourceCondition"))
+				buff.variables.Add("resourceCondition", new List<int>());
+
+			foreach (var resource in player.resourceReel)
+			{
+				//걍랜덤
+			}
+			
+		}	
+		private static void Exposure(Buff buff, GamePlayer player)
+		{
+			if (buff.variables.ContainsKey("cardExposureIndexList"))
+				buff.variables.Add("cardExposureIndexList", new List<int>());
+			
+		}	
+		private static void BreakDown(Buff buff, GamePlayer player)
+		{
+
+		}	
+		private static void HighEfficiency(Buff buff, GamePlayer player)
+		{
+
+		}	
+		private static void LowEfficiency(Buff buff, GamePlayer player)
+		{
+
+		}	
+	}
+}

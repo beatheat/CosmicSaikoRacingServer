d8d835ff5f020f45283d009a9f89881133de94ef
beatheat <oh@beathe.at>
2023-04-12 23:41:03 +0900

    change: tcp->udp변경

--- MatchingServer/MatchingServer/MatchingServer.cs
+++ MatchingServer/MatchingServer/MatchingServer.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Security.Cryptography;
 using EdenNetwork;
 using System.Runtime.CompilerServices;
+using EdenNetwork.Udp;
 
 namespace MatchingServer
 {
@@ -18,12 +19,12 @@ namespace MatchingServer
             public bool canceled = false;
         }
 
-        public EdenNetServer server;
+        public EdenUdpServer server;
         public Dictionary<int, string> room;
         public Queue<MatchInfo> matchQueue;
         public Dictionary<string, MatchInfo> matchMap;
 
-        public MatchingServer(EdenNetServer server)
+        public MatchingServer(EdenUdpServer server)
         {
             this.server = server;
             room = new Dictionary<int, string>();
@@ -38,42 +39,42 @@ namespace MatchingServer
             server.AddResponse("DestroyLobby", DestroyLobby);
             server.AddResponse("GetRoomAddress", GetRoomAddress);
 
-            Task.Run(async () =>
-            {
-                while (true)
-                {
-                    if (matchQueue.Count >= 4)
-                    {
-                        List<MatchInfo> matchGroup = new List<MatchInfo>();
-                        for (int i = 0; i < 4; i++)
-                        {
-                            var clientInfo = matchQueue.Dequeue();
-                            if (!clientInfo.canceled && server.StillConnected(clientInfo.id))
-                                matchGroup.Add(clientInfo);
-                        }
-                        if (matchGroup.Count != 4)
-                        {
-                            foreach (var clientInfo in matchGroup)
-                                matchQueue.Enqueue(clientInfo);
-                            continue;
-                        }
-
-                        var hostAddress = StringToAddress(matchGroup[0].id);
-                        server.Send("StartHost", matchGroup[0].id);
-                        for (int i = 1; i < 4; i++)
-                        {
-                            server.Send("StartClient", matchGroup[i].id, hostAddress);
-                        }
-                        for (int i = 0; i < 4; i++)
-                        {
-                            if (matchMap.ContainsKey(matchGroup[i].id))
-                                matchMap.Remove(matchGroup[i].id);
-                            //server.DisconnectClient(matchGroup[i].id);
-                        }
-                    }
-                    await Task.Delay(100);
-                }
-            });
+            // Task.Run(async () =>
+            // {
+            //     while (true)
+            //     {
+            //         if (matchQueue.Count >= 4)
+            //         {
+            //             List<MatchInfo> matchGroup = new List<MatchInfo>();
+            //             for (int i = 0; i < 4; i++)
+            //             {
+            //                 var clientInfo = matchQueue.Dequeue();
+            //                 if (!clientInfo.canceled && server.StillConnected(clientInfo.id))
+            //                     matchGroup.Add(clientInfo);
+            //             }
+            //             if (matchGroup.Count != 4)
+            //             {
+            //                 foreach (var clientInfo in matchGroup)
+            //                     matchQueue.Enqueue(clientInfo);
+            //                 continue;
+            //             }
+            //
+            //             var hostAddress = StringToAddress(matchGroup[0].id);
+            //             server.Send("StartHost", matchGroup[0].id);
+            //             for (int i = 1; i < 4; i++)
+            //             {
+            //                 server.Send("StartClient", matchGroup[i].id, hostAddress);
+            //             }
+            //             for (int i = 0; i < 4; i++)
+            //             {
+            //                 if (matchMap.ContainsKey(matchGroup[i].id))
+            //                     matchMap.Remove(matchGroup[i].id);
+            //                 //server.DisconnectClient(matchGroup[i].id);
+            //             }
+            //         }
+            //         await Task.Delay(100);
+            //     }
+            // });
 
             server.Listen(99999, (string client_id) =>
             {
@@ -94,7 +95,7 @@ namespace MatchingServer
             }
             if (!data.TryGet<string>(out var privateAddress))
                 return EdenData.Error("Wrong formatted address number");
-            
+              
             int roomNum;
             do
             {
@@ -102,37 +103,38 @@ namespace MatchingServer
             } while (room.ContainsKey(roomNum));
 
             room.Add(roomNum, privateAddress + "," + clientId);
-            Task.Run(HeartBeat);
+
             return new EdenData(roomNum);
             
-            async void HeartBeat()
-            {
-                Console.WriteLine("Ready to Login to GameServer");
-                var address = clientId.Split(":");
-                EdenNetClient client = new EdenNetClient(address[0], int.Parse(address[1]));
-                bool connection = true;
-                client.SetServerDisconnectEvent(() =>
-                {
-                    connection = false;
-                });
-                int count = 0;
-                while (await client.ConnectAsync(1) != ConnectionState.OK)
-                {
-                    Console.WriteLine($"Trying Login {count}");
-                    count++;
-                    if (count > 10)
-                    {
-                        Console.WriteLine($"GameServer connection fail on {clientId}");
-                        return;
-                    }
-                }
-                Console.WriteLine($"GameServer connection success on {clientId}");
-                while (connection)
-                {
-                    await client.SendAsync("HeartBeat");
-                    await Task.Delay(3000);
-                }
-            }
+            // async void HeartBeat()
+            // {
+            //     Console.WriteLine("Ready to Login to GameServer");
+            //     var address = clientId.Split(":");
+            //     EdenUdpClient client = new EdenUdpClient(address[0], int.Parse(address[1]));
+            //     bool connection = true;
+            //     client.SetDisconnectEvent(() =>
+            //     {
+            //         connection = false;
+            //     });
+            //     int count = 0;
+            //     while (await client.ConnectAsync() != ConnectionState.OK)
+            //     {
+            //         Console.WriteLine($"Trying Login {count}");
+            //         await Task.Delay(300);
+            //         count++;
+            //         if (count > 10)
+            //         {
+            //             Console.WriteLine($"GameServer connection fail on {clientId}");
+            //             return;
+            //         }
+            //     }
+            //     Console.WriteLine($"GameServer connection success on {clientId}");
+            //     while (connection)
+            //     {
+            //         await client.SendAsync("HeartBeat");
+            //         await Task.Delay(3000);
+            //     }
+            // }
         }
 
         public EdenData DestroyLobby(string clientId, EdenData data)
@@ -160,9 +162,6 @@ namespace MatchingServer
                 return new EdenData(new EdenError("ERR:Unauthorized Access"));
             if (room.ContainsKey(roomNum))
             {
-                // if (clientId.Contains("192.168"))
-                //     return new EdenData(StringToAddress("127.0.0.1:17979"));
-                // else
                 string privateAddress = room[roomNum].Split(",")[0];
                 string publicAddress = room[roomNum].Split(",")[1];
                 return new EdenData(new Dictionary<string, object>
--- MatchingServer/MatchingServer/Program.cs
+++ MatchingServer/MatchingServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
+using EdenNetwork.Udp;
 
 namespace MatchingServer
 {
@@ -10,11 +11,9 @@ namespace MatchingServer
         static void Main(string[] args)
         {
             Console.Title = "CosmicSaikoRacing - MatchingServer";
-            EdenNetServer server = new EdenNetServer(16969, "MatchingServerLog.txt");
+            EdenUdpServer server = new EdenUdpServer(16969, "MatchingServerLog.txt");
             MatchingServer matchingServer = new MatchingServer(server);
             matchingServer.Run();
-
-  
 
             Console.WriteLine("Ctrl+c to close server");
             Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs eventArgs) =>
